83eeadc9fb0948c0422a4db79846aa212f274ae9
Evgeniy Kovalev <49534555+ekovalew@users.noreply.github.com>
2021-03-02 22:28:10 +0300

    Update Bot Notify.py

--- Bot Notify.py
+++ Bot Notify.py
@@ -1,30 +1,62 @@
 import requests
-from pprint import pprint
 import telebot
+import os
+from dotenv import load_dotenv, find_dotenv
+import logging
+import time
 
-chat_id = '1023420974'
-bot = telebot.TeleBot('1642990318:AAHkfnOuwg4fcDJycdbdhJOhKq4qSNWTc34');
+logging.basicConfig(filename="sample.log", level=logging.INFO)
 
-header = {'Authorization': 'Token cdcf22419df29b18f8d6f8e77262b923151e01ff'}
-param = {}
-while True:
-    try:
+load_dotenv(find_dotenv())
+token = os.getenv("TOKEN")
+chat_id = os.getenv("CHAT_ID")
+bot = telebot.TeleBot(token);
+token_dm = os.getenv("TOKEN_DEVMAN")
+
+header = {'Authorization': token_dm}
+
+
+def main():
+    param = {}
+    while True:
         response = requests.get("https://dvmn.org/api/long_polling/", headers=header, params=param)
-        if response.json()['status'] == 'timeout':
-            timestamp = response.json()['timestamp_to_request']
-            param = {'timestamp': timestamp}
-        else:
-            param = {}
-            lesson = response.json()['new_attempts'][0]['lesson_title']
-            url = 'https://dvmn.org' + response.json()['new_attempts'][0]['lesson_url']
-            result = response.json()['new_attempts'][0]['is_negative']
-            if result is True:
-                bot.sendMessage(chat_id, "Преподаватель проверил работу [" + lesson +  "](" + url + ")!\n К сожалению, в работе нашлись ошибки", parse_mode='Markdown')
+        respJSON = response.json()
+        decoded_response = respJSON
+        try:
+            response.raise_for_status()
+            if 'error' in decoded_response:
+                raise requests.exceptions.HTTPError(decoded_response['error'])
+            status = respJSON['status']
+            if status == 'timeout':
+                timestamp = respJSON['timestamp_to_request']
+                param = {'timestamp': timestamp}
             else:
-                bot.send_message(chat_id=chat_id,
-                                 text="Преподаватель проверил работу [" + lesson +  "](" + url + ")!\n Преподавателю все понравилось, можно приступать к следующему уроку", parse_mode='Markdown')
-        pprint(response.json())
-    except requests.exceptions.ReadTimeout:
-        continue
-    except ConnectionError:
-        continue
+                timestamp = respJSON['last_attempt_timestamp']
+                param = {'timestamp': timestamp}
+                new_attempt = respJSON['new_attempts'][0]
+                lesson = new_attempt['lesson_title']
+                url = 'https://dvmn.org' + new_attempt['lesson_url']
+                result = new_attempt['is_negative']
+                if result:
+                    bot.send_message(chat_id=chat_id,
+                                     text=f"Преподаватель проверил работу [{lesson}]({url})!\n К сожалению, в работе нашлись ошибки",
+                                     parse_mode='Markdown')
+                else:
+                    bot.send_message(chat_id=chat_id,
+                                     text=f"Преподаватель проверил работу [{lesson}]({url})!\n Преподавателю все понравилось, можно приступать к следующему уроку",
+                                     parse_mode='Markdown')
+        except Exception as e:
+            print('Ошибка при подключении: ' + str(e))
+            logging.error(f'{str(e)}')
+            break
+        except requests.exceptions.ReadTimeout:
+            logging.error('ReadTimeout')
+            time.sleep(2)
+            continue
+        except ConnectionError:
+            logging.error('ConnectionError')
+            time.sleep(2)
+            continue
+
+if __name__ == '__main__':
+    main()
