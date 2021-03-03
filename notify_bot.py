9a1a01aeca750da014210e2921f55bff9a3d2a3f
Evgeniy Kovalev <49534555+ekovalew@users.noreply.github.com>
2021-03-03 23:05:31 +0300

    Update notify_bot.py

--- notify_bot.py
+++ notify_bot.py
@@ -5,57 +5,53 @@ from dotenv import load_dotenv, find_dotenv
 import logging
 import time
 
-logging.basicConfig(filename="sample.log", level=logging.INFO)
-
-load_dotenv(find_dotenv())
-token = os.getenv("TOKEN")
-chat_id = os.getenv("CHAT_ID")
-bot = telebot.TeleBot(token);
-token_dm = os.getenv("TOKEN_DEVMAN")
-
-header = {'Authorization': token_dm}
+logger = logging.getLogger(__file__)
 
 
 def main():
+    logging.basicConfig(filename="sample.log", level=logging.INFO)
+    load_dotenv(find_dotenv())
+    TOKEN = os.getenv("TOKEN")
+    CHAT_ID = os.getenv("CHAT_ID")
+    bot = telebot.TeleBot(TOKEN);
+    TOKEN_DVMN = os.getenv("TOKEN_DEVMAN")
+    header = {'Authorization': TOKEN_DVMN}
     param = {}
+
     while True:
-        response = requests.get("https://dvmn.org/api/long_polling/", headers=header, params=param)
-        respJSON = response.json()
-        decoded_response = respJSON
         try:
+            response = requests.get("https://dvmn.org/api/long_polling1/", headers=header, params=param)
             response.raise_for_status()
-            if 'error' in decoded_response:
-                raise requests.exceptions.HTTPError(decoded_response['error'])
-            status = respJSON['status']
+            response_dec = response.json()
+            status = response_dec['status']
             if status == 'timeout':
-                timestamp = respJSON['timestamp_to_request']
+                timestamp = response_dec['timestamp_to_request']
                 param = {'timestamp': timestamp}
             else:
-                timestamp = respJSON['last_attempt_timestamp']
+                timestamp = response_dec['last_attempt_timestamp']
                 param = {'timestamp': timestamp}
-                new_attempt = respJSON['new_attempts'][0]
+                new_attempt = response_dec['new_attempts'][0]
                 lesson = new_attempt['lesson_title']
-                url = 'https://dvmn.org' + new_attempt['lesson_url']
+                url_base = 'https://dvmn.org'
+                slug = new_attempt['lesson_url']
+                url = f'{url_base}{slug}'
                 result = new_attempt['is_negative']
                 if result:
-                    bot.send_message(chat_id=chat_id,
+                    bot.send_message(chat_id=CHAT_ID,
                                      text=f"Преподаватель проверил работу [{lesson}]({url})!\n К сожалению, в работе нашлись ошибки",
                                      parse_mode='Markdown')
                 else:
-                    bot.send_message(chat_id=chat_id,
+                    bot.send_message(chat_id=CHAT_ID,
                                      text=f"Преподаватель проверил работу [{lesson}]({url})!\n Преподавателю все понравилось, можно приступать к следующему уроку",
                                      parse_mode='Markdown')
-        except Exception as e:
-            logging.error(f'{str(e)}')
+        except requests.exceptions.HTTPError:
+            logger.exception('Ошибка')
             break
         except requests.exceptions.ReadTimeout:
-            logging.error('ReadTimeout')
+            logger.exception('Ошибка')
             time.sleep(2)
             continue
-        except ConnectionError:
-            logging.error('ConnectionError')
-            time.sleep(2)
-            continue
+
 
 if __name__ == '__main__':
     main()
